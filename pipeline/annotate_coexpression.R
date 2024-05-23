71e7a986f795891379e7a2590db89f8aa4303d8a
marie-minaeva <minaeva.mav@gmail.com>
2024-05-23 16:50:57 +0200

    add annotation with coexpression patterns

---
+++ pipeline/annotate_coexpression.R
@@ -0,0 +1,13 @@
+library(data.table)
+library(dplyr)
+
+tf_target_mapping = fread("data/regulons/TF_target_mapping_filtered_merged_K562_with_ppi_with_dnase_with_atac_with_motifs_with_ccres.tsv", nThread=10)
+coexpr_mat = fread("data/coexpression/Bicor_NT_weissmann_K562.tsv")
+coexpr_adj_list = melt(coexpr_mat, id.vars=c("V1"))
+colnames(coexpr_adj_list) = c("tf", "gene_symbol", "corr")
+coexpr_adj_list %>% filter((tf != gene_symbol) & 
+				(tf %in% unique(tf_target_mapping$tf))) -> coexpr_adj_list
+left_join(tf_target_mapping, coexpr_adj_list, by=c("tf", "gene_symbol")) %>% 
+	mutate(is_coexpressed = (!is.na(corr) & (abs(corr) > 0.1))) -> tf_target_mapping
+fwrite(tf_target_mapping, "data/regulons/TF_target_mapping_filtered_merged_K562_with_ppi_with_dnase_with_atac_with_motifs_with_ccres.tsv", sep="\t", col.names=T, row.names=F, quote=F)
+
