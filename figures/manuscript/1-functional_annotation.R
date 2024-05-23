bc5a344c1b1ce5995338a28146e2c535821ae0d4
marie-minaeva <minaeva.mav@gmail.com>
2024-05-23 15:31:20 +0200

    update of annotation groups

--- figures/manuscript/1-functional_annotation.R
+++ figures/manuscript/1-functional_annotation.R
@@ -19,56 +19,63 @@ source("figures/fig_style.R")
 library(RColorBrewer)
 cell_line = c("K562", "HepG2", "MCF7", "GM12878")
 foreach(cl = cell_line) %do% {
-        input_file <- sprintf("data/1-functional_annotation/%s_regulon_with_motifs.tsv", cl)
+    #input_file <- sprintf("data/regulons/%s_all_regulons.tsv", cl)
+	input_file <- sprintf("data/regulons/TF_target_mapping_filtered_merged_%s_with_ppi_with_dnase_with_atac_with_motifs_with_ccres.tsv", cl)
 
 	to_plot = fread(input_file)
+	
+	to_plot <- to_plot %>% dplyr::mutate(pair = paste(tf, gene_symbol, sep = "_")) %>% filter(is_S2Mb | is_S2Kb | is_M2Kb)
 
 	test_list = list("S2Mb" = to_plot%>% 
-				  filter(is_method_1) %>% 
-				  dplyr::select(pair) %>%
-				  distinct() %>%
-				  unlist(),
-			"M2Kb" = to_plot %>%
-                	          filter(is_method_2) %>%
-                        	  dplyr::select(pair) %>%
-	                          distinct() %>%
-        	                  unlist(),
-			"S2Kb" = to_plot %>%
-                        	  filter(is_method_3) %>%
-	                          dplyr::select(pair) %>%
-        	                  distinct() %>%
-                	          unlist(),
-			"S2Mb_motif" = to_plot %>%
-        	                  filter(is_motif_method1) %>%
-                	          dplyr::select(pair) %>%
-                        	  distinct() %>%
-	                          unlist(),
-			"M2Kb_motif" = to_plot %>%
-                	          filter(is_motif_method2) %>%
-                        	  dplyr::select(pair) %>%
-                          	distinct() %>%
-	                          unlist(),
-			"S2Kb_motif" = to_plot %>%
-                	          filter(is_motif_method3) %>%
-                        	  dplyr::select(pair) %>%
-	                          distinct() %>%
-        	                  unlist()
-	)
+                    				  filter(is_S2Mb) %>% 
+                    				  dplyr::select(pair) %>%
+                    				  distinct() %>%
+                    				  unlist(),
+        			"M2Kb" = to_plot %>%
+                        	          filter(is_M2Kb) %>%
+                                	  dplyr::select(pair) %>%
+        	                          distinct() %>%
+                	                  unlist(),
+        			"S2Kb" = to_plot %>%
+                                	  filter(is_S2Kb) %>%
+        	                          dplyr::select(pair) %>%
+                	                  distinct() %>%
+                        	          unlist(),
+        			"is_motif" = to_plot %>%
+                	                  filter(!is.na(n_motifs) | ! is.na(n_motifs_hoco)) %>%
+                        	          dplyr::select(pair) %>%
+                                	  distinct() %>%
+        	                          unlist(),
+	                        "is_PLS" = to_plot %>%
+                                          filter(is_pls) %>%
+                                          dplyr::select(pair) %>%
+                                          distinct() %>%
+                                          unlist(),
+                                "is_pELS" = to_plot %>%
+                                          filter(is_pels) %>%
+                                          dplyr::select(pair) %>%
+                                          distinct() %>%
+                                          unlist(),
+                                "is_dELS" = to_plot %>%
+                                          filter(is_dels) %>%
+                                          dplyr::select(pair) %>%
+                                          distinct() %>%
+                                          unlist()
+			)
 
         plot_file <- sprintf("plots/1-functional_annotation/overlap_annotations_%s.svg", cl)
 	svg(plot_file)
 
-	p = upset(fromList(test_list), sets = c(#"DNAse", "ATAC", 
-						"S2Mb", "M2Kb", 
-			"S2Kb", "S2Mb_motif", "M2Kb_motif", "S2Kb_motif"), 
+	p = upset(fromList(test_list), sets = c("S2Mb", "M2Kb", 
+			"S2Kb", "is_motif", "is_PLS", "is_pELS", "is_dELS"), 
 		mb.ratio = c(0.7, 0.3),
         	order.by="freq",
 	        mainbar.y.label = "Number of TF-target gene pairs",
 		sets.x.label = "TF-target\ninteractiosn",
 	        show.numbers = F,
         	text.scale = 2,
-	        scale.sets = "log10",
-		nintersects = 12,
+	        scale.sets = "identity",
+		nintersects = 15,
 		intersection)
 	print(p)
 	dev.off()
@@ -84,19 +91,4 @@ n
 p$New_data %>% filter((S2Mb == 1) & (S2Kb == 1) & (M2Kb == 1)) %>% nrow
 # 0.3164567 percent of interactions confirmed by 3 approaches, DNAse and ATAC-Seq
 
-#p$New_data %>% filter((S2Mb == 1) & (S2Kb == 1) & (M2Kb == 1) & (ATAC == 1) & (DNAse ==1)) %>% nrow    
-# 0.3164567 percent of interactions confirmed by 3 approaches, DNAse and ATAC-Seq
-
-#p$New_data %>% filter((S2Mb == 1) & (S2Kb == 1) & (M2Kb == 1) & (ATAC == 1) & (DNAse ==1)) %>% nrow / n
-# 0.3164567 percent of interactions confirmed by 3 approaches, DNAse and ATAC-Seq
-
-#p$New_data %>% filter((S2Mb == 1) & (S2Kb == 0) & (M2Kb == 0) & (ATAC == 0) & (DNAse == 0)) %>% nrow / n
-# 0.2585541 percent of interactions confirmed only by S2Mb regulon
-
-#p$New_data %>% filter((S2Mb == 1) & (S2Kb == 0) & (M2Kb == 0) & (ATAC == 1) & (DNAse == 1)) %>% nrow / n
-# 0.2100381 percent of interactions confirmed by S2Mb, DNAse and ATAC-Seq
-
-
-p$New_data %>% filter(((S2Mb == 1) & (S2Mb_motif ==1)) | ((M2Kb == 1) & (M2Kb_motif ==1)) | ((S2Kb == 1) & (S2Kb_motif ==1))) %>% nrow  / n
-# 0.1077216 percent of interactions enriched in TFBS
 
